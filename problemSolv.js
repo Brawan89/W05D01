847997745b9e73b348493be8a3c9a0f382514990
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-07 22:51:24 +0300

    file

--- problemSolv.js
+++ problemSolv.js
@@ -1,8 +1,4 @@
 function isPalindrome(x) {
    x = x.replace(/[.,?:;\/() _-]/g, '').toLowerCase();
     return x == x.split('').reverse().join('');
-    //  x = x.replace(/\W/g , '');
-    //  x = x.toLowerCase()
-
-    //  return x === x.reverse()
 }
